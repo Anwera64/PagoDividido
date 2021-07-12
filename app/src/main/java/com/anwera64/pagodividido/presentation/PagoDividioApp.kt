2f406cc1fbe99171222ffac1aaaf0242d9410644
jos miguel zea guerrero <jzeaguerrero@gmail.com>
2021-07-12 16:34:11 -0500

    feature : add injection with koin

--- app/src/main/java/com/anwera64/pagodividido/presentation/PagoDividioApp.kt
+++ app/src/main/java/com/anwera64/pagodividido/presentation/PagoDividioApp.kt
@@ -1,15 +1,12 @@
 package com.anwera64.pagodividido.presentation
 
 import android.app.Application
-import com.anwera64.pagodividido.data.AppDatabase
-import com.anwera64.pagodividido.domain.repository.CompanionRepository
-import com.anwera64.pagodividido.domain.repository.TripRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 
 class PagoDividioApp : Application() {
 
-    val database by lazy { AppDatabase.getDatabase(this) }
-    val tripRepository by lazy { TripRepository(database.tripDao()) }
-    val companionRepository by lazy { CompanionRepository(database.companionDao()) }
+    override fun onCreate() {
+        super.onCreate()
+        initDI()
+    }
+
 }
