f7a301ed10abd1fc2dad5b882b0af315dcf054f8
Anton <atch97@gmail.com>
2021-07-15 22:02:23 -0500

    feature: added dynamic debts for each expenditure.

---
+++ app/src/main/java/com/anwera64/pagodividido/utils/uicomponents/DebtorInputView.kt
@@ -0,0 +1,56 @@
+package com.anwera64.pagodividido.utils.uicomponents
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.StringRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.anwera64.pagodividido.R
+import com.anwera64.pagodividido.databinding.LayoutDebtorAmountInputBinding
+
+class DebtorInputView(context: Context) : ConstraintLayout(context) {
+
+    private val binding: LayoutDebtorAmountInputBinding
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        binding = DataBindingUtil.inflate(inflater, R.layout.layout_debtor_amount_input, this, true)
+        binding.cbCompanion.setOnCheckedChangeListener { _, isChecked ->
+            isInputHidden = !isChecked
+        }
+    }
+
+    var name: String
+        set(value) {
+            binding.cbCompanion.text = value
+        }
+        get() = binding.cbCompanion.text.toString()
+
+    var isInputHidden: Boolean = true
+        set(value) {
+            field = value
+            binding.tilAmountOwed.visibility = if (field) View.GONE else View.VISIBLE
+        }
+
+    fun isChecked(): Boolean = binding.cbCompanion.isChecked
+
+    fun getAmount(): Double = binding.tiAmountOwed.text.toString().toDoubleOrNull() ?: 0.0
+
+    fun setText(text: String) = binding.tiAmountOwed.setText(text)
+
+    fun setText(@StringRes text: Int) = binding.tiAmountOwed.setText(text)
+
+    fun setError(@StringRes text: Int) {
+        binding.tilAmountOwed.error = context.getString(text)
+    }
+
+    fun setError(text: String) {
+        binding.tilAmountOwed.error = text
+    }
+
+    fun setErrorEnabled(isEnabled: Boolean) {
+        binding.tilAmountOwed.isErrorEnabled = isEnabled
+    }
+}
