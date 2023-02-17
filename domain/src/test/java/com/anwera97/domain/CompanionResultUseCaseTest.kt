28f9ac564ed7ccf6f45e6574c835a9163894d20f
Anton Soares <atch97@gmail.com>
2023-02-17 14:07:31 -0500

    added unit tests for companion result use case.

--- domain/src/test/java/com/anwera97/domain/CompanionResultUseCaseTest.kt
+++ domain/src/test/java/com/anwera97/domain/CompanionResultUseCaseTest.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertNull
 import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -48,6 +49,96 @@ class CompanionResultUseCaseTest {
             companion = payer,
             totalPaid = 0.0,
             debts = mutableMapOf(debtorName to 5.0),
+            isExtended = false
+        )
+
+        // Setup
+        whenever(companionRepository.getResultInfoFor(tripId)) doReturn flow { emit(debtList) }
+
+        // Exec
+        val emittedResult = companionResultUseCase.getPayersWithDebtors(tripId, payerId).first()
+        assertEquals(emittedResult, expectedResult)
+    }
+
+    @Test
+    fun `get no result with wrong id`() = runTest {
+        // Data
+        val tripId = 1
+        val payerId = 1
+        val payer = CompanionModel("2", "")
+        val debtList = listOf(
+            ResultModel(
+                companion = payer,
+                totalPaid = 0.0,
+                debts = mutableMapOf("" to 10.0),
+                isExtended = false
+            ),
+        )
+
+        // Setup
+        whenever(companionRepository.getResultInfoFor(tripId)) doReturn flow { emit(debtList) }
+
+        // Exec
+        val emittedResult = companionResultUseCase.getPayersWithDebtors(tripId, payerId).first()
+        assertNull(emittedResult)
+    }
+
+    @Test
+    fun `get no result when trip emits no results`() = runTest {
+        // Data
+        val tripId = 1
+        val payerId = 1
+        val debtList: List<ResultModel> = emptyList()
+
+        // Setup
+        whenever(companionRepository.getResultInfoFor(tripId)) doReturn flow { emit(debtList) }
+
+        // Exec
+        val emittedResult = companionResultUseCase.getPayersWithDebtors(tripId, payerId).first()
+        assertNull(emittedResult)
+    }
+
+    @Test
+    fun `get result with three companions`() = runTest {
+        // Data
+        val tripId = 1
+        val payerId = 1
+        val payerName = "Name 1"
+        val payer = CompanionModel(payerId.toString(), payerName)
+        val firstDebtorName = "Name 2"
+        val firstDebtor = CompanionModel("", firstDebtorName)
+        val secondDebtorName = "Name 3"
+        val secondDebtor = CompanionModel("", secondDebtorName)
+        val debtList = listOf(
+            ResultModel(
+                companion = payer,
+                totalPaid = 0.0,
+                debts = mutableMapOf(
+                    firstDebtorName to 10.0,
+                    secondDebtorName to 5.0
+                ),
+                isExtended = false
+            ),
+            ResultModel(
+                companion = firstDebtor,
+                totalPaid = 0.0,
+                debts = mutableMapOf(payerName to 5.0),
+                isExtended = false
+            ),
+            ResultModel(
+                companion = secondDebtor,
+                totalPaid = 0.0,
+                debts = mutableMapOf(payerName to 10.0),
+                isExtended = false
+            )
+        )
+        val expectedResult = ResultModel(
+            companion = payer,
+            totalPaid = 0.0,
+            debts = mutableMapOf(
+                firstDebtorName to 5.0,
+                secondDebtorName to -5.0
+            ),
             isExtended = false
         )
 
